e2c87b4e11d2556769b8fc23db3569c10695b4f1
Shivam19114 <shivamkumarabc2006@gmail.com>
2026-06-16 16:05:39 +0530

    Set up FastAPI backend

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "AI Interview Coach Backend Running"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
